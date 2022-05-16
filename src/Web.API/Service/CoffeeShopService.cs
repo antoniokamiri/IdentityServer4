31c7aceea0efa3c7e0495c219bd1c820e5496f99
Antonio Kamiri <antoniokamiri@gmail.com>
2022-05-16 22:59:41 +0300

    Services

---
+++ src/Web.API/Service/CoffeeShopService.cs
@@ -0,0 +1,42 @@
+﻿using DataAcess.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.API.Models;
+
+namespace Web.API.Service
+{
+    public class CoffeeShopService : ICoffeeShopService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoffeeShopService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CoffeeShopModel>> List()
+        {
+            var coffeeShop = await _context.CoffeeShops.ToListAsync();
+
+            var coffeeModelList = new List<CoffeeShopModel>();
+
+
+            foreach (var item in coffeeShop)
+            {
+                var coffeeModel = new CoffeeShopModel
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    OpeningHours = item.OpeningHours,
+                    Address = item.Address,
+                };
+                coffeeModelList.Add(coffeeModel);
+            }
+
+
+            return coffeeModelList;
+        }
+    }
+}
---
+++ src/Web.API/Service/ICoffeeShopService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Web.API.Models;
+
+namespace Web.API.Service
+{
+    public interface ICoffeeShopService
+    {
+        Task<List<CoffeeShopModel>> List();
+    }
+}

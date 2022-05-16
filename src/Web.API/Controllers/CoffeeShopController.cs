b9123632db41f1569d4160a90a019b270f133f4d
Antonio Kamiri <antoniokamiri@gmail.com>
2022-05-16 22:59:14 +0300

    Controllers

---
+++ src/Web.API/Controllers/CoffeeShopController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Web.API.Service;
+
+namespace Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeShopController : Controller
+    {
+        private readonly ICoffeeShopService _coffeeShopService;
+        private readonly ILogger<CoffeeShopController> _logger;
+
+        public CoffeeShopController(ICoffeeShopService coffeeShopService, ILogger<CoffeeShopController> logger)
+        {
+            _coffeeShopService = coffeeShopService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var coffeeShop = await _coffeeShopService.List();
+
+            return Ok(coffeeShop);
+        }
+    }
+}

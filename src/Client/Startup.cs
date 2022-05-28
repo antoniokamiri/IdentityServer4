68c4242a1cc20d1ee762008be898dd9d1fb78d5b
Antonio Kamiri <antoniokamiri@gmail.com>
2022-05-28 10:26:22 +0300

    Identity Server Client

--- src/Client/Startup.cs
+++ src/Client/Startup.cs
@@ -1,4 +1,7 @@
 
+using Client.Service;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -30,6 +33,29 @@ namespace Client
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddHttpClient();
+
+            services.Configure<IdentityServerSettings>(Configuration.GetSection("IdentityServerSettings"));
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(
+                OpenIdConnectDefaults.AuthenticationScheme,
+                options =>
+                {
+                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.SignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                    options.Authority = Configuration["InteractiveServiceSetting:AuthorityUrl"];
+                    options.ClientId = Configuration["InteractiveServiceSetting:ClientId"];
+                    options.ClientSecret = Configuration["InteractiveServiceSetting:ClientSecrets"];
+                    options.ResponseType = "code";
+                    options.UsePkce = true;
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +76,9 @@ namespace Client
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {

bf5c150a77afc8125b17fd05600689b52672f8ef
Antonio Kamiri <antoniokamiri@gmail.com>
2022-05-28 10:27:10 +0300

    Controller WebApi

--- src/Web.API/Controllers/CoffeeShopController.cs
+++ src/Web.API/Controllers/CoffeeShopController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using Web.API.Service;
@@ -7,6 +8,7 @@ namespace Web.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class CoffeeShopController : Controller
     {
         private readonly ICoffeeShopService _coffeeShopService;
